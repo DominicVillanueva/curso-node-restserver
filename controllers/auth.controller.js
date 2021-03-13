1ec17edb6bc4cd836edcab3c4a648d74a1458ac0
DominicVillanueva <partner.dominic@hotmail.com>
2021-03-13 13:33:48 -0500

    Google Sign added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const UserModel = require('../models/user.model');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generateJWT");
+
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
     const { email, password } = req.body;
@@ -44,6 +46,52 @@ const login = async(req, res = response) => {
     }
 };
 
+const googleSignIn = async(req = request, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const {name, img, email} = await googleVerify(id_token);
+
+        // Verificar si el correo existe
+        let user = await UserModel.findOne({ email });
+        if(!user) {
+            // Tengo que crearlo
+            const data = {
+                name,
+                email,
+                password: ':)',
+                img,
+                google_signin: true
+            };
+
+            user = new UserModel(data);
+            await user.save();
+        }
+
+        // Si el usuario se encuentra en status = false.
+        if(!user.status) {
+            return res.status(401).json({
+                msg: 'Usuario bloqueado.',
+            });
+        };
+
+        // Genear el JWT
+        const token = await generarJWT(user.id);
+
+        res.json({
+            user,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg:'Token de google no es válido',
+        });
+    }
+
+    
+};
+
 module.exports = {
     login,
+    googleSignIn,
 }
